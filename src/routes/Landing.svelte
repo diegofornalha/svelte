0c1c3df2d584acffe32e13675e4f69cd2e86390e
Diego Fornalha <diegofornalha@gmail.com>
2023-10-11 18:58:17 -0300

    remove: svg

--- src/routes/Landing.svelte
+++ src/routes/Landing.svelte
@@ -2,9 +2,7 @@
     import { push } from "svelte-spa-router";
 
     import LogoSvelte from "../assets/svelte.svg";
-    import LogoAppwrite from "../assets/appwrite.svg";
     import LogoGithub from "../assets/github.svg";
-    import LogoTwitter from "../assets/twitter.svg";
 </script>
 
 <!-- Hero -->
@@ -16,12 +14,6 @@
         <h1 class="text-4xl md:text-7xl lg:text-9xl font-bold">toTooooDoooo</h1>
         <p class="my-8 text-xl md:text-2xl lg:text-3xl font-medium">
             A Simple To-do App built with
-            <img
-                class="w-8 h-8 inline"
-                src={LogoAppwrite}
-                alt="Appwrite Logo"
-            />
-            Appwrite and
             <img class="w-8 h-8 inline" src={LogoSvelte} alt="Svelte Logo" /> Svelte
         </p>
         <button
@@ -40,28 +32,6 @@
     >
         <a href="http://github.com/appwrite/appwrite">
             <img class="w-10 h-10 inline" src={LogoGithub} alt="GitHub Logo" />
-        </a>
-    </div>
-    <div
-        class="rounded-full mx-4 transition duration-200 ease-in-out transform hover:-translate-y-3 hover:scale-125 hover:shadow-2xl"
-    >
-        <a href="https://twitter.com/appwrite_io">
-            <img
-                class="w-10 h-10 inline"
-                src={LogoTwitter}
-                alt="Twitter Logo"
-            />
-        </a>
-    </div>
-    <div
-        class="rounded-full mx-4 transition duration-200 ease-in-out transform hover:-translate-y-3 hover:scale-125 hover:shadow-2xl"
-    >
-        <a href="http://appwrite.io">
-            <img
-                class="w-10 h-10 inline"
-                src={LogoAppwrite}
-                alt="Appwrite Logo"
-            />
         </a>
     </div>
 </section>
