b1dc582dbaed143da9604dedbc6a3ae1f0971e24
Diego Fornalha <diegofornalha@gmail.com>
2023-10-11 19:51:58 -0300

    Update Todo.svelte
    coloquei ele no canto superior direito

--- src/routes/Todo.svelte
+++ src/routes/Todo.svelte
@@ -52,7 +52,7 @@
   </div>
 </section>
 
-<section class="absolute bottom-0 right-0 py-3 px-6 mr-8 mb-8">
+<section class="fixed top-4 right-4">
   <button
     on:click={logout}
     class="mx-auto mt-4 py-3 px-12 font-semibold text-md rounded-lg shadow-md bg-white text-gray-900 border border-gray-900 hover:border-transparent hover:text-white hover:bg-gray-900 focus:outline-none"
