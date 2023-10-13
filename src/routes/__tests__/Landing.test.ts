fe11c75eae0a70740524c001bee1c14d11276ec4
Diego Fornalha <diegofornalha@gmail.com>
2023-10-13 03:50:10 -0300

    Delete Landing.test.ts

--- src/routes/__tests__/Landing.test.ts
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/svelte';
-import Landing from '../Landing.svelte';
-
-describe('Landing page', () => {
-  it('should render without crashing', () => {
-    const { container } = render(Landing);
-    expect(container).toMatchSnapshot();
-  });
-});
